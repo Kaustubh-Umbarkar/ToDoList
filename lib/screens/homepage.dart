73c887745f0b3ae68fbecf3d31d1f0df8a5540d3
Kaustubh-Umbarkar <kaustubhumbarkar36@gmail.com>
2021-03-10 15:22:36 +0530

    Completed

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/database_helper.dart';
 import 'package:todo/screens/taskpage.dart';
-import 'package:todo/screens/widgets.dart';
+
+
+import '../widgets.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -9,66 +12,94 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  DatabaseHelper _dbHelper = DatabaseHelper();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          padding: EdgeInsets.symmetric(
-            horizontal: 24.0,
-
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           color: Color(0xFFF6F6F6),
-          child:Stack(
+          child: Stack(
             children: [
               Column(
-                crossAxisAlignment:CrossAxisAlignment.start ,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
                     margin: EdgeInsets.only(
                       top: 32.0,
                       bottom: 32.0,
                     ),
-                    child: Image(image: AssetImage(
-                      "assets/images/logo.png"
-                    ),
+                    child: Image(
+                      image: AssetImage('assets/images/logo.png'),
                     ),
                   ),
                   Expanded(
-                    child: ListView(
-                      children: [
-                        TaskCardWidget(
-                          title: "Hello there",
-                          desc: "You did it",
-                        ),
-                        TaskCardWidget(),
-                        TaskCardWidget(),
-                        TaskCardWidget(),
-                        TaskCardWidget(),
-                      ],
+                    child: FutureBuilder(
+                      initialData: [],
+                      future: _dbHelper.getTasks(),
+                      builder: (context, snapshot) {
+                        return ScrollConfiguration(
+                          behavior: NoGlowBehaviour(),
+                          child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Taskpage(
+                                        task: snapshot.data[index],
+                                      ),
+                                    ),
+                                  ).then(
+                                        (value) {
+                                      setState(() {});
+                                    },
+                                  );
+                                },
+                                child: TaskCardWidget(
+                                  title: snapshot.data[index].title,
+                                  desc: snapshot.data[index].description,
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
                     ),
                   )
                 ],
-
               ),
               Positioned(
-                bottom: 20.0,
-                  right: 0.0,
+                bottom: 24.0,
+                right: 0.0,
                 child: GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=> Taskpage()
-                    ),
-                    );
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Taskpage(
+                            task: null,
+                          )),
+                    ).then((value) {
+                      setState(() {});
+                    });
                   },
                   child: Container(
                     width: 60.0,
-                      height: 60.0,
-                    decoration:BoxDecoration(
-                      color: Color(0xFF7349FE),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
+                          begin: Alignment(0.0, -1.0),
+                          end: Alignment(0.0, 1.0)),
                       borderRadius: BorderRadius.circular(20.0),
-                    ) ,
-                    child:Image(
+                    ),
+                    child: Image(
                       image: AssetImage(
                         "assets/images/add_icon.png",
                       ),
@@ -83,4 +114,5 @@ class _HomepageState extends State<Homepage> {
     );
   }
 }
+
 
